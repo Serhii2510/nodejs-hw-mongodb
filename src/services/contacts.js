fa6ad4e37d1e14fc352c13871c88783f7c9bc308
Serhii Ivashchenko <s.ivashchenko.1989@gmail.com>
2024-10-10 11:20:26 +0300

    homework done

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,4 +1,4 @@
-import { contactsCollection } from "../db/contacts.js";
+import { contactsCollection } from "../db/models/contacts.js";
 
 export const getAllContacts = async () => {
   const contacts = await contactsCollection.find();
@@ -7,5 +7,37 @@ export const getAllContacts = async () => {
 
 export const getContactById = async (contactId) => {
   const contact = await contactsCollection.findById(contactId);
+  return contact;
+};
+
+export const createContact = async (payload) => {
+  const contact = await contactsCollection.create(payload);
+  return contact;
+};
+
+export const updateContact = async (contactId, payload, options = {}) => {
+  const rawResult = await contactsCollection.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
+
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    contact: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
+};
+
+export const deleteContact = async (contactId) => {
+  const contact = await contactsCollection.findOneAndDelete({
+    _id: contactId,
+  });
+
   return contact;
 };
